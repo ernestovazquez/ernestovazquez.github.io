afa212519998aa84349e4516cdeb77a733a64427
Ernesto Vázquez <vazquezgarciaernesto@gmail.com>
2024-07-08 10:08:38 +0200

    Update copy-link.js

--- assets/js/_commons/copy-link.js
+++ assets/js/_commons/copy-link.js
@@ -7,7 +7,7 @@
  */
 
 function copyLink(url) {
-  if (!url || 0 === url.length) {
+  if (!url || url.length === 0) {
     url = window.location.href;
   }
   
@@ -16,4 +16,15 @@ function copyLink(url) {
   $temp.val(url).select();
   document.execCommand("copy");
   $temp.remove();
+
+  // Cambiar el texto del botón temporalmente
+  var originalText = "Copy link";
+  var newText = "Link copiado";
+  var $copyBtn = $(".copy-button"); // Asume que tienes un botón con la clase .copy-button
+
+  $copyBtn.text(newText);
+
+  setTimeout(function() {
+    $copyBtn.text(originalText);
+  }, 2000); // Cambia el texto de vuelta después de 2 segundos (2000 milisegundos)
 }
